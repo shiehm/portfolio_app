35bac6394f3bac855b63a1f83b10f6f8dcc7ee9e
Melvin Shieh <melvin@Melvins-Air.lan>
2025-12-02 13:35:46 -0500

    rebased schema, added css

---
+++ schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE accounts (
+    id serial PRIMARY KEY,
+    account_name text NOT NULL,
+    account_type text NOT NULL
+);
+
+CREATE TABLE assets (
+    id serial PRIMARY KEY,
+    ticker text NOT NULL,
+    name text NOT NULL,
+    category text NOT NULL,
+    current_price numeric(10,2) NOT NULL
+);
+
+CREATE TABLE holdings (
+    id serial PRIMARY KEY,
+    asset_id integer NOT NULL REFERENCES assets(id) ON DELETE CASCADE,
+    account_id integer NOT NULL REFERENCES accounts(id) ON DELETE CASCADE,
+    shares integer NOT NULL DEFAULT 0
+);
